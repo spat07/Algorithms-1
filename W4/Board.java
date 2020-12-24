846261db907d54c9357f5e804848b9a271a9e70b
spat07 <50160270+spat07@users.noreply.github.com>
2020-12-24 17:24:25 +0530

    final changes

--- W4/Board.java
+++ W4/Board.java
@@ -1,1 +1,251 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 
+public class Board {
+	
+	private int[][] board;
+	private final int n;
+	
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles)
+    {
+    	
+    	if (tiles == null)
+    		throw new IllegalArgumentException("Null input argument!");
+
+    	// create new init board
+    	n = tiles.length;
+    	
+    	if(n < 1)
+    		throw new IllegalArgumentException("Unacceptable board size!");
+    	
+    	board = new int[n][n];
+    	for (int i = 0; i < n; i++)
+    	{
+    		for (int j = 0; j < n; j++)
+    		{
+    			board[i][j] = tiles[i][j];
+    		}
+    	}
+    	
+    }
+
+    // string representation of this board
+    public String toString()
+    {
+    	String str = new String();
+    	str = n + "\n";
+    	
+    	for(int i = 0; i < n; i++)
+    	{
+    		for(int j = 0; j < n; j++)
+    		{
+    			str = str.concat(board[i][j] + " ");
+    		}
+    		str = str.concat("\n");
+    	}
+    	
+    	return str;
+    }
+    
+    
+
+    // board dimension n
+    public int dimension()
+    {
+    	return n;
+    }
+
+    // number of tiles out of place
+    public int hamming()
+    {
+    	int h = 0;
+    	int count = 1;//goalboard is linear seq
+    	
+    	for (int i = 0; i < n; i++)
+    	{
+    		for(int j = 0; j < n; j++)
+    		{
+    			if(board[i][j] > 0 && board[i][j] != count)
+    				h++;
+    			count++;
+    		}
+    	}
+
+    	return h;
+    }
+
+    
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan()
+    {
+    	int m = 0;
+    	int count = 1;
+    	for (int i = 0; i < n; i++)
+    	{
+    		for(int j = 0; j < n; j++)
+    		{
+    			if(board[i][j] > 0 && board[i][j] != count)
+    			{
+    				double val = (double) board[i][j];
+    				int gr = (int)(Math.ceil(val/n) - 1); //row idx
+    				int gc = (int)(val - gr*n - 1); // column idx
+    				m += (Math.abs(i - gr) + Math.abs(j - gc));
+    			}
+    			count++;
+    		}
+    	}
+    	return m;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal()
+    {
+    	int count = 1;
+    	final int lim = n * n;
+    	for(int i = 0; i < n; i++)
+    	{
+    		for(int j = 0; j < n; j++)
+    		{
+    			if(board[i][j] != (count%lim))
+    				return false;
+    			count++;
+    		}
+    	}
+    	
+    	return true;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y)
+    {
+    	
+    	if (y == null)
+    		throw new NullPointerException();
+
+    	if(!this.getClass().equals(y.getClass()))
+    		throw new ClassCastException();
+    	
+    	Board that = (Board)y;
+    	
+    	if(this.n != that.n)
+    		return false;
+
+    	if (Arrays.deepEquals(this.board, that.board))
+    		return true;
+    	else
+    		return false;
+    }
+    
+    private void swap(int zx, int zy, int zxx, int zyy)
+    {
+    	int temp = this.board[zx][zy];
+    	this.board[zx][zy] = this.board[zxx][zyy];
+    	this.board[zxx][zyy] = temp;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors()
+    {
+    	int zx = -1, zy = -1;
+    	Stack<Board> nb = new Stack<Board>();
+    	
+    	// find position of the zero, and check for valid neighbors
+    	for(int i = 0; i < n; i++)
+    		for(int j = 0; j < n; j++)
+    			if(board[i][j] == 0)
+    			{
+    				zx = i;
+    				zy = j;
+    				i = j = n;
+    			}
+    	// generate board for valid neighbors
+    	if(zx - 1 >= 0)
+    	{
+    		Board b1 = new Board(board);
+    		b1.swap(zx, zy, zx - 1, zy);
+    		nb.push(b1);
+    	}
+		if(zy - 1 >= 0)
+		{
+    		Board b2 = new Board(board);
+    		b2.swap(zx, zy, zx, zy - 1);
+    		nb.push(b2);
+		}
+    	
+    	if(zx + 1 < n)
+    	{
+    		Board b1 = new Board(board);
+    		b1.swap(zx, zy, zx + 1, zy);
+    		nb.push(b1);
+    	}
+		if(zy + 1 < n)
+		{
+    		Board b2 = new Board(board);
+    		b2.swap(zx, zy, zx, zy + 1);
+    		nb.push(b2);
+		}
+
+    	
+    	return nb;
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin()
+    {
+    	Board tb = new Board(board);
+
+    	if (n > 1)
+    	{    	
+	    	for(int i = 0; i < n; i++)
+	    	{
+	    		if (tb.board[i][0] > 0 && tb.board[i][1] > 0)
+	    		{
+	    			int t = tb.board[i][0];
+	    			tb.board[i][0] = tb.board[i][1];
+	    			tb.board[i][1] = t;
+	    			break;
+	    		}
+	    			
+	    	}
+    		
+    	}
+    	return tb;
+    	
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args)
+    {
+    	In in = new In(args[0]);
+    	
+    	int n = in.readInt();
+    	
+    	int[][] tiles = new int[n][n];
+    	for (int i = 0; i < n; i++)
+    	{
+    		for (int j = 0; j < n; j++)
+    		{
+    			tiles[i][j] = in.readInt();
+    		}
+    	}
+    	
+    	Board initial = new Board(tiles);
+    	System.out.print(initial.toString());
+    	System.out.printf("Hamming=%d\n", initial.hamming());
+    	System.out.printf("Manhattan=%d\n", initial.manhattan());
+    	System.out.println(initial.twin().toString());
+    	
+    	
+    	int[][] tile1 = { {1,2,3,4}, {5,6,7,8},{9,10,11,12}, {13,14,15,0}};
+    	Board next = new Board(tile1);
+    	System.out.print(next.toString());
+    	System.out.printf("Hamming=%d\n", next.hamming());
+    	System.out.printf("Manhattan=%d\n", next.manhattan());
+    	System.out.println(next.twin().toString());
+    	
+    }
+
+}
--- W4/Solver.java
+++ W4/Solver.java
@@ -1,1 +1,233 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.In;
 
+public class Solver {
+	
+	private Stack<Board> res = null;
+	private int moves;
+	private boolean solvable = true;
+	
+	//comparators
+	private static final Comparator<SearchNode> BY_HAM = new ByHam();
+	private static class ByHam implements Comparator<SearchNode>
+	{
+		public int compare(SearchNode a, SearchNode b)
+		{
+			int v1 = a.Current.hamming() + a.moves;
+			int v2 = b.Current.hamming() + b.moves;
+			if (v1 < v2)
+				return -1;
+			else if (v1 > v2)
+				return 1;
+			else
+				return 0;
+		}
+	}
+
+	private static final Comparator<SearchNode> BY_MAN = new ByMan();
+	private static class ByMan implements Comparator<SearchNode>
+	{
+		public int compare(SearchNode a, SearchNode b)
+		{
+			int v1 = a.Current.manhattan() + a.moves;
+			int v2 = b.Current.manhattan() + b.moves;
+			if (v1 < v2)
+				return -1;
+			else if (v1 > v2)
+				return 1;
+			else
+				return 0;
+		}
+	}
+
+	private class SearchNode{
+		SearchNode Prev;
+		Board Current;
+		int moves;
+		
+		
+		public SearchNode(SearchNode p, Board c, int m)
+		{
+			Prev = p;
+			Current = c;
+			moves = m;
+		}
+		
+
+	}
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial)
+    {
+    	// MinPQ Q
+    	MinPQ<SearchNode> pq = new MinPQ<SearchNode>(BY_MAN);
+    	//MinPQ<SearchNode> pq = new MinPQ<SearchNode>(BY_HAM);
+    	
+    	// MinPQ for twin
+    	MinPQ<SearchNode> tpq = new MinPQ<SearchNode>(BY_MAN);
+    	//MinPQ<SearchNode> tpq = new MinPQ<SearchNode>(BY_HAM);
+    	
+    	SearchNode node = new SearchNode(null, initial, 0);//start with 0 moves
+    	SearchNode tNode = new SearchNode(null, initial.twin(), 0);
+    	
+    	// Insert initial board to min PQ
+    	pq.insert(node);
+    	
+    	//insert twin
+    	tpq.insert(tNode);
+    	
+    	// until we find a solution - BFS
+    	//System.out.println("Solving");
+    	//int iter = 0;
+    	//while(iter <= 500000)
+    	while(true)
+    	{
+    		SearchNode n = pq.delMin();
+    		
+    		//System.out.println("-----------------------------Normal----------------------------------");
+    		//System.out.printf("Selected choice: score = %d + %d\n", n.Current.hamming(), moves);
+    		//System.out.printf("Slected choice: score = %d + %d\n", n.Current.manhattan(), n.moves);
+    		//System.out.println(n.Current.toString());
+    		
+    		if(n.Current.isGoal())
+    		{
+    			moves = n.moves;
+    			//System.out.printf("total iterations to solve %d\n", iter);
+    			res = new Stack<Board>();
+    			// create iterable
+    			while(n != null)
+    			{
+    				res.push(n.Current);
+    				n = n.Prev;
+    			}
+
+    			break;
+    		}
+    		
+    		//System.out.printf("Adding neighbors, moves = %d\n", n.moves + 1);
+    		moves = n.moves + 1;
+    		// compute neighbors(Board class), find min from PQ
+    		for(Board b : n.Current.neighbors())
+    		{
+    			// avoid queueing, if its already in the queue
+    			if(!isExisting(b, n))
+    			{
+    				//System.out.println(b.toString());
+    				//System.out.printf("score = %d + %d\n", b.manhattan(), n.moves + 1);
+    				//System.out.printf("score = %d + %d\n", b.hamming(), n.moves + 1);
+    				SearchNode nb = new SearchNode(n,b,n.moves + 1);
+    				pq.insert(nb);
+    			}
+    		}
+
+    		
+    		// check feasibility for twin board
+    		SearchNode tn = tpq.delMin();
+
+    		//System.out.println("-----------------------------twin----------------------------------");;
+    		//System.out.printf("Selected choice: score = %d + %d\n", tn.Current.hamming(), tn.moves);
+    		//System.out.printf("Slected choice: score = %d + %d\n", tn.Current.manhattan(), tn.moves);
+    		//System.out.println(tn.Current.toString());
+    		
+    		if(tn.Current.isGoal())
+    		{
+    			// board is unsolvable
+    			//System.out.println("twin board solved!");
+    			solvable = false;
+    			moves = -1;
+    			break;
+    		}
+    		
+    		for(Board b : tn.Current.neighbors())
+    		{
+    			if(!isExisting(b, tn))
+    			{
+    				//System.out.println(b.toString());
+    				//System.out.printf("score = %d + %d\n", b.manhattan(), n.moves + 1);
+    				//System.out.printf("score = %d + %d\n", b.hamming(), n.moves + 1);
+    				SearchNode nb = new SearchNode(tn, b, tn.moves + 1);
+    				tpq.insert(nb);
+    			}
+    		}
+    		
+    		//tracker
+    		//System.out.printf("%c", '.');
+    		//if(iter++ % 80 == 0)
+    		//	System.out.println("\n");
+    	}
+    	
+    	
+    	//if(iter >= 500000)
+    	//	System.out.println("!!****reached limit*****!!");
+
+    }
+
+    private boolean isExisting(Board b, SearchNode n)
+    {
+
+    	while(n != null)
+    	{
+    		if(b.equals(n.Current))
+    			return true;
+    		n = n.Prev;
+    	}
+    	return false;
+    }
+    // is the initial board solvable? (see below)
+    public boolean isSolvable()
+    {
+    	return solvable;
+    }
+
+    // min number of moves to solve initial board
+    public int moves()
+    {
+    	return moves;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution()
+    {
+    	// Keep list of selected neighbors
+    	return res;
+    }
+
+    // test client (see below) 
+    public static void main(String[] args)
+    {
+    	In in = new In(args[0]);
+    	
+    	int n = in.readInt();
+    	
+    	int[][] tiles = new int[n][n];
+    	for (int i = 0; i < n; i++)
+    	{
+    		for (int j = 0; j < n; j++)
+    		{
+    			tiles[i][j] = in.readInt();
+    		}
+    	}
+
+    	Board tb = new Board(tiles);
+
+    	//System.out.println("**** Input ******");
+    	//System.out.println(tb.toString());
+    	
+    	Solver sol = new Solver(tb);
+    	if(sol.isSolvable())
+    	{
+    		//System.out.println("!!!!***** Solution****!!!!");
+    		System.out.printf("Minimum number of moves = %d\n", sol.moves());
+    		for (Board t : sol.solution())
+    		{
+    			System.out.println(t.toString());
+    		}
+    	}
+    	else
+    	{
+    		System.out.println("No solution possible!");
+    	}
+    }
+
+}
