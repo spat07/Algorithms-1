968917d8fd65d7d5f1b5d56e2fd3393d71d21cdd
spat07 <50160270+spat07@users.noreply.github.com>
2021-01-09 12:51:38 +0530

    added all methods and few test cases

--- W5/src/PointSET.java
+++ W5/src/PointSET.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
@@ -37,31 +38,69 @@ public class PointSET {
    public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle (or on the boundary)
    {
        Stack<Point2D> res = new Stack<Point2D>();
-       
+       for(Point2D p : pset)
+       {
+           if(rect.contains(p))
+               res.push(p);
+       }
        return (res);
    }
-   public Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
+   public Point2D nearest(Point2D that)             // a nearest neighbor in the set to point p; null if the set is empty
    {
-       return (null);
+       double dist = 1.0;
+       Point2D res = null;
+       for(Point2D p : pset)
+       {
+           if(p.distanceTo(that) < dist)
+           {
+               dist = p.distanceTo(that);
+               res = p;
+           }
+       }
+       return (res);
    }
 
    public static void main(String[] args)                  // unit testing of the methods (optional)
    {
-       PointSET ps1 = new PointSET();
+
+       RectHV rc1 = new RectHV(0.0, 0.0, 1.0, 1.0);
+       StdDraw.setPenRadius(0.01);
+       rc1.draw();
        
-       Point2D p1 = new Point2D(0.4, 0.4);
+       PointSET ps1 = new PointSET();
+       StdDraw.setPenRadius(0.02);
+       
+       Point2D p1 = new Point2D(0.25, 0.5);
        ps1.insert(p1);
        
-       Point2D p2 = new Point2D(0.3, 0.4);
+       Point2D p2 = new Point2D(0.25, 1.0);
        ps1.insert(p2);
        
-       Point2D p3 = new Point2D(0.4, 0.5);
+       Point2D p3 = new Point2D(0.5, 0.75);
        ps1.insert(p3);
        
-       Point2D p4 = new Point2D(0.3, 0.5);
+       Point2D p4 = new Point2D(0.5, 0.25);
        ps1.insert(p4);
        
        System.out.printf("%d points in the set\n", ps1.size());
        ps1.draw();
+       
+       Point2D op1 = new Point2D(0.75, 0.75);
+       StdDraw.setPenColor(StdDraw.RED);
+       op1.draw();
+       
+       System.out.printf("Nearest pt is %s\n", ps1.nearest(op1).toString());
+       
+       RectHV rc2 = new RectHV(0.25, 0.25, 0.75, 0.5);
+       StdDraw.setPenRadius(0.01);
+       StdDraw.setPenColor(StdDraw.BLUE);
+       rc2.draw();
+       
+       System.out.printf("Point in the rectHV %s\n", rc2.toString());
+       for(Point2D p : ps1.range(rc2))
+       {
+           System.out.println(p.toString());
+       }
+       
    }
 }
